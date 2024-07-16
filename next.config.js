670f5b439d2d62277337e3fec2ae4d6826863ceb
Nguyễn Minh Hiếu <hieu24313@gmail.com>
2024-07-16 11:13:33 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  basePath: isProd ? "/zapps/4433292384685420599" : undefined,
+  basePath: isProd ? "/zapps/2928403159849082663" : undefined,
   assetPrefix: isProd ? "https://hieu24313.github.io/nextjs-mini" : undefined,
   output: 'export', // Thêm dòng này để hỗ trợ xuất tĩnh
 };
